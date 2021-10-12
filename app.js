bcbb302d074ad9aebec453509496943377c3947d
Rafi <99.rafiindrajati@gmail.com>
2021-10-12 18:53:26 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(()=>{
+    console.log('2 s timer')
+}, 2000) //miliseconds
+
+setTimeout(()=>{
+    console.log('0 s timer')
+}, 0) //miliseconds
+
+console.log('Stopping')
