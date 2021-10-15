2887c6e056872ac91d865a3c5b122857a719790a
Rafi Indrajati <99.rafiindrajati@gmail.com>
2021-10-15 10:38:52 +0700

    http reqiest

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(()=>{
-    console.log('2 s timer')
-}, 2000) //miliseconds
+const url = 'http://api.weatherstack.com/current?access_key=2c94d99fc0d6151701d3b572a165ac87&query=37.8267,-122.42333'
 
-setTimeout(()=>{
-    console.log('0 s timer')
-}, 0) //miliseconds
+request({url: url},(error, response)=>{
+    // console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
+// console.log('Starting')
 
-console.log('Stopping')
+// setTimeout(()=>{
+//     console.log('2 s timer')
+// }, 2000) //miliseconds
+
+// setTimeout(()=>{
+//     console.log('0 s timer')
+// }, 0) //miliseconds
+
+// console.log('Stopping')
