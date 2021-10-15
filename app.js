ee5b6e0b902f734d470cac47d89012a28820930c
Rafi Indrajati <99.rafiindrajati@gmail.com>
2021-10-15 16:40:11 +0700

    add geocode url

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=2c94d99fc0d6151701d3b572a165ac87&query=37.8267,-122.42333'
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmFmaWluZHJhaiIsImEiOiJja3VydXB2dHY1OTR4MndtYXpyZDJvYWdmIn0.3tbbXmWrx1smiRSG8uZDVw&limit=1'
 
-request({url: url, json: true},(error, response)=>{
+request({url: geocodeURL, json: true},(error, response)=>{
     // console.log(response)
     // const data = JSON.parse(response.body)
-    console.log(response.body.current)
+    // console.log(response.body.current)
+    // const datatempr = response.body.current.temperature
+    // const weather_desc = response.body.current.weather_descriptions[0]
+    // const feelslike = response.body.current.feelslike
+    // const precip = response.body.current.precip
+    // console.log('it is currently', weather_desc, 'It feels like',feelslike,'Celcius. And there is',precip,"% of rain")
+
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
 })
 // console.log('Starting')
 
